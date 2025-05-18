73937bf186845ec835cc09ead9a1734de57988c9
Murtaza stove wala <162013428+Murtaza-stovewala@users.noreply.github.com>
2025-05-18 20:41:12 +0530

    Update login.js
    changed server

--- student/login/login.js
+++ student/login/login.js
@@ -13,7 +13,7 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/students/login", {
+      const res = await fetch("https://smart-attendance-system-2p2j.onrender.com/api/students/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ collegeId, password })
