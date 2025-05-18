5df9a53957057697f263882a0cd67a58d586ba9a
Murtaza stove wala <162013428+Murtaza-stovewala@users.noreply.github.com>
2025-05-18 20:40:22 +0530

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -10,7 +10,7 @@ document.getElementById("collegeForm").addEventListener("submit", async (e) => {
   }
 
   try {
-    const res = await fetch("http://localhost:5000/api/admin/add-student", {
+    const res = await fetch("https://smart-attendance-system-2p2j.onrender.com/api/admin/add-student", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ collegeId, name })
